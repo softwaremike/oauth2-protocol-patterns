c5efe5b175ede001b1ca69359b6a81d65a162a69
Joe Grandja <jgrandja@pivotal.io>
2020-05-26 20:42:52 -0400

    Add scope-based authorization

--- microservice-a/src/main/java/sample/config/ResourceServerConfig.java
+++ microservice-a/src/main/java/sample/config/ResourceServerConfig.java
@@ -1,11 +1,11 @@
 /*
- * Copyright 2002-2019 the original author or authors.
+ * Copyright 2002-2020 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *      https://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -31,11 +31,10 @@ public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
         http
             .authorizeRequests(authorizeRequests ->
                 authorizeRequests
-                        .mvcMatchers("/service-b/**").access("hasAuthority('SCOPE_authority-b')"))
+                    .mvcMatchers("/service-a/**").access("hasAuthority('SCOPE_authority-a')"))
             .oauth2ResourceServer(oauth2ResourceServer ->
                 oauth2ResourceServer
-                    .jwt())
-            .oauth2Client();
+                    .jwt());
     }
     // @formatter:on
 }
